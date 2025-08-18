ab4ebecae7067f715f806d1c2b7d2921647bf724
misunders2d <114879787+misunders2d@users.noreply.github.com>
2025-08-18 22:46:51 +0300

    feat: add telegram_utils.py

---
+++ personal_clone/utils/telegram_utils.py
@@ -0,0 +1,5 @@
+# This file will contain the utility functions for interacting with the Telegram API.
+
+def get_telegram_client():
+  # This function will initialize and return a Telegram client.
+  pass
