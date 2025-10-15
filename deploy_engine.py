29ea78d7c710fb63356ccd3ed786e00c2ce7ff24
misunderstood <2djohar@gmail.com>
2025-10-15 19:56:24 +0300

    adding agent_config file to deploy with nmp install

--- deploy_engine.py
+++ deploy_engine.py
@@ -1,5 +1,5 @@
 import vertexai
-from vertexai import agent_engines # For the prebuilt templates
+from vertexai import agent_engines  # For the prebuilt templates
 from typing import Dict
 
 from personal_clone import config, agent
@@ -9,21 +9,23 @@ vertexai.init(
     project=config.GOOGLE_CLOUD_PROJECT,
     location=config.GOOGLE_CLOUD_LOCATION,
     staging_bucket=config.GOOGLE_CLOUD_STORAGE_BUCKET,
-    credentials=config.get_identity_token()
+    credentials=config.get_identity_token(),
 )
 
-resource_name = "projects/131221610297/locations/us-east4/reasoningEngines/3013365547548016640"
-requirements = 'requirements.txt'
-display_name= 'personal_clone'
-gcs_dir_name='gs://personalclone'
-extra_packages = ['personal_clone/', 'installation_scripts/install.sh']
-env_vars: Dict = dotenv_values('personal_clone/.env')
-env_vars.pop('GOOGLE_CLOUD_PROJECT')
-env_vars.pop('GOOGLE_CLOUD_LOCATION')
+resource_name = (
+    "projects/131221610297/locations/us-east4/reasoningEngines/3013365547548016640"
+)
+requirements = "requirements.txt"
+display_name = "personal_clone"
+gcs_dir_name = "gs://personalclone"
+extra_packages = ["personal_clone/", "installation_scripts/install.sh"]
+env_vars: Dict = dotenv_values("personal_clone/.env")
+env_vars.pop("GOOGLE_CLOUD_PROJECT")
+env_vars.pop("GOOGLE_CLOUD_LOCATION")
 build_options: Dict = {"installation_scripts": ["installation_scripts/install.sh"]}
 
 
-#create
+# create
 remote_agent = agent_engines.create(
     agent_engine=agent.root_agent,
     requirements=requirements,
@@ -33,7 +35,6 @@ remote_agent = agent_engines.create(
     env_vars=env_vars,
     build_options=build_options,
 )
-
 
 
 # #update
